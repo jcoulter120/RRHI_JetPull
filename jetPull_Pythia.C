54f6673a5b2c7b2e94447019e00a83ed89dffbd5
Jennifer Coulter <jennifer.coulter@cern.ch>
2016-04-20 22:22:31 +0200

    Added to new repo for organization

--- jetPull_Pythia.C
+++ jetPull_Pythia.C
@@ -5,6 +5,8 @@
 // Test macro for plotting the jet pull vector. 
 //
 
+//CURRENTLY : This macro attempts to compute the jet pull vector of the leading jet, then calculates the pull angle of that jet with all subjets. 
+
 #include <iostream>
 #include <stdio.h>
 #include <fstream>
@@ -40,43 +42,9 @@
 
 using namespace std;
 
-//PLANE CLASS =======================================================
-class Plane{
-public:
-  TVector3 v1, v2, proj, u1, u2;
-  Double_t scalar1, scalar2, mag1, mag2; 
-  Plane(TVector3);
-
-  //returns a projection onto the 2D plane 
-  TVector3 Projection(TVector3 jaxis){
-    //Find the projection of a jet onto this subspace
-    if(v1.Mag() == 0) { scalar1 = 0; }   else { scalar1 = jaxis.Dot(v1)/(v1.Dot(v1)); } 
-    if(v2.Mag() == 0) { scalar2 = 0; }   else { scalar2 = jaxis.Dot(v2)/(v2.Dot(v2)); } 
-    v1 = scalar1*v1;
-    v2 = scalar2*v2;
-    proj(0) = v1(0) + v2(0);
-    proj(1) = v1(1) + v2(1);
-    proj(2) = v1(2) + v2(2); 
-    
-    return proj;
-  }//end of projection
-};
-//plane class constructor
-Plane::Plane(TVector3 nT){
-  
-  //Use TVector3 to find an orthogonal vector and a second vector orthogonal to the first and nT
-  v1 = nT.Orthogonal();  v2 = nT.Cross(v1);
-
-  //Normalize, checking for 0 length axes
-  if ((v1(0) == 0) && (v1(1) == 0) && (v1(2) == 0)){  v1(0) = 0;    v1(1) = 0;    v1(2) = 0; }
-  else { mag1 = v1.Mag();   v1(0) = v1(0)/mag1;    v1(1) = v1(1)/mag1;    v1(2) = v1(2)/mag1; } 
-  if ((v2(0) == 0) && (v2(1) == 0) && (v2(2) == 0)){  v2(0) = 0;    v2(1) = 0;    v2(2) = 0; } 
-  else { mag2 = v2.Mag();   v2(0) = v2(0)/mag2;    v2(1) = v2(1)/mag2;    v2(2) = v2(2)/mag2; }
-}//end plane constructor
-//END PLANE CLASS =======================================================
 
 // DIVIDE BY BIN WIDTH ==================================================
-//creates histograms in terms of Thrust vs. dN/dT
+//creates histograms in terms of variable vs. dN/dT
 TH1F* DivideByBinWidth(TH1F * hist, const char * name){
 
   TH1F* h_return = new TH1F(name, "", hist->GetNbinsX(), 0,1);
@@ -104,10 +72,10 @@ TVector3 Norm(TVector3 v){
 
 //BEGIN MAIN MACRO =====================================================
 void jetPull_Pythia(Int_t startfile = 0,
-		   Int_t endfile = 22,
-		   Int_t jobNumber = 100,
+		   Int_t endfile = 44,
+		   Int_t jobNumber = 3,
 		   int radius = 3,
-		   float ptCutLead = 60.0,
+		   float ptCutLead = 30.0,
 		   float ptCut = 30.0,
 		   float etaCut = 2.0){
 
@@ -127,26 +95,24 @@ void jetPull_Pythia(Int_t startfile = 0,
   TTree * weight;
 
   //DEFINE HISTOGRAMS +++++++++++++++++++++++++++++++++++
-  TH1F * h_thrust = new TH1F("thrust_unscaled", "", 50,0,1);
-  TH1F * h_min = new TH1F("thrust_min", "", 50,0,1);
-  TH1F * h_maj = new TH1F("thrust_maj", "", 50,0,1);
-  
-  TH1F * h_jetPull = new TH1F("jetPull", "", 50,0,1);
-  
-  TH1F * h_pT = new TH1F("pT", "", 100, 0, 500);
+  TH1F * h_jetPull = new TH1F("jetPull", "", 50,0,1);  
+  TH1F * h_leadingJetpT = new TH1F("leading_pT", "", 100, 0, 500);
   TH1F * h_pTcut = new TH1F("pTcut", "", 100, 0, 500);
   TH1F * h_nref = new TH1F("nref", "", 12, 0, 12);
   TH1F * h_jetCount = new TH1F("jetCount", "", 12, 0, 12);
-  TH1F * h_eta = new TH1F("eta", "", 60, -2, 2);
-  TH1F * h_phi = new TH1F("phi", "", 60, -3.15, 3.15);
+  TH1F * h_leadingJeteta = new TH1F("leadingJeteta", "", 60, -2, 2);
+  TH1F * h_leadingJetphi = new TH1F("leadingJetphi", "", 60, -3.15, 3.15);
   TH1F * h_weight = new TH1F("weighted", "", 1200, 0, 1200);
   TH1F * h_unweight = new TH1F("unweighted", "", 1200, 0, 1200);
   TH1F * h_etaParticle = new TH1F("etaParticle", "", 60, -3, 3);
   TH1F * h_yParticle = new TH1F("yParticle", "", 60, -3, 3);
   TH2F * h_jetPull_2d = new TH2F("jetPull_2d","Jet pT vs Eta for pT>20",30,-3,+3,60,-3.5,3.5);
   TH2D * h_jetPull_3d = new TH2D("jetPull_3d","phi vs y vs pT" ,30,-3,+3,60,-3.5,3.5);
-  //TH1F * pTCutoffs = new TH1F("pTcutoffs", "Jet Pull Angle -- with pThat Cutoffs", 60, 0.01, 500);
-  //TH1F * pullAngle = new TH1F("pullAngle", "Jet Pull Angle vs pT", 60, -3.5, 3.5); 
+  TH1F * h_30 = new TH1F("pTcutoff30", "Jet Pull Angle -- with pThat Cutoffs", 60, 0.01, 500);
+  TH1F * h_60 = new TH1F("pTcutoff60", "Jet Pull Angle -- with pThat Cutoffs", 60, 0.01, 500);
+  TH1F * h_90 = new TH1F("pTcutoff90", "Jet Pull Angle -- with pThat Cutoffs", 60, 0.01, 500);
+  TH1F * h_120 = new TH1F("pTcutoff120", "Jet Pull Angle -- with pThat Cutoffs", 60, 0.01, 500);
+  TH1F * h_pullAngle = new TH1F("pullAngle", "Jet Pull Angle vs pT", 60, -3.5, 3.5); 
 
   //DEFINE TREE VARIABLES +++++++++++++++++++++++++++++++++++
   Float_t pt[1000];  
@@ -157,8 +123,6 @@ void jetPull_Pythia(Int_t startfile = 0,
   Float_t genphi[1000];
   Float_t geny[1000];
   Float_t geneta[1000];
-
-  TVector2 jetPull;
   
   Int_t nref;
   Int_t ngen;
@@ -171,29 +135,16 @@ void jetPull_Pythia(Int_t startfile = 0,
   Int_t noise;
   
   Double_t pThat_weight;
-  Float_t pThat; 
+  Float_t pThat;
 
   //DEFINE VARS FOR CALCULATION +++++++++++++++++++++++++++++
-  Float_t dot = 0;
-  Double_t mag = 0;
-  Double_t thrust_temp = 0;
-  Double_t thrust_max = 0;
-  Double_t dot_maj = 0;
-  Double_t dot_min = 0;
-  Double_t min_temp = 0;
-  Double_t maj_temp = 0;
-  Double_t thrust_maj_max =0;
-  Double_t thrust_min_max = 0;
-  TVector3 max_thrust_axis;
-  TVector3 p3Norm;
-  TVector2 r; 
-  Float_t max_eta = 0;   Float_t temp_eta = 0;  
-  Float_t max_phi = 0;   Float_t temp_phi = 0;
-  Int_t max_nref;
-  Int_t jetCount = 0;
+  TVector2 r[1000];
+  TVector2 jetPull;
+  Int_t leadJet = 0;
   Int_t eventCount = 0;
   Float_t jetPullMag  = 0;
-  Float_t pullAngle = 0; 
+  Float_t pullAngle = 0;
+  Float_t dipolarity = 0;
 
   //LOAD IN FILE NAMES +++++++++++++++++++++++++++++++++++
   string input_file = "Text_Files/pp_MC_HiForest.txt";
@@ -212,13 +163,11 @@ void jetPull_Pythia(Int_t startfile = 0,
   //BEGIN FILE LOOP ======================================================
   for(int ifile = startfile; ifile < endfile; ifile++){
     
-    string s = "";
-    string str = s.append(filename[ifile]);
+    string str = filename[ifile];
     file = TFile::Open(str.c_str());
     string w = Form("weights/weights_pp_%d.root", ifile+1);
     cout << w << endl; 
     weight_file = TFile::Open(w.c_str());
-    //if(weighted) weight_file = TFile::Open(w.c_str());
 
     if (debug) cout << "\n **** =========================== New File ================================= **** \n ";
     cout << "File Name: " << filename[ifile] << endl;
@@ -226,6 +175,7 @@ void jetPull_Pythia(Int_t startfile = 0,
 
     //define trees and file +++++++++++++++++
     t = (TTree*)file->Get(Form("ak%dPFJetAnalyzer/t", radius));
+    return;
     hiEvt = (TTree*)file->Get("hiEvtAnalyzer/HiTree");
     skim = (TTree*)file->Get("skimanalysis/HltTree");
     weight = (TTree*)weight_file->Get("weights");
@@ -273,23 +223,28 @@ void jetPull_Pythia(Int_t startfile = 0,
       hiEvt->GetEntry(nentry);
       weight->GetEntry(nentry);
       
-      jetCount = 0;
       if(TMath::Abs(vz) > 15 || halo == 0) {
-	//isGoodEvt = 0;
-	continue;
+	isGoodEvt = 0;
+      	continue;
       }
-      //isGoodEvt = 1;
       
       // get the pt vector for each event which passes you jet selections based on pT and eta
       vector <float> pt_v;
       vector <float> eta_v;
       vector <float> phi_v;
-
+      Double_t ptLead = 0;
+      
       for(int ij = 0; ij<nref; ++ij){
-	if(pt[ij] > ptCutLead && fabs(eta[ij]) < etaCut){
-	  isGoodEvt = 1; 
+	if(pt[ij] > ptCutLead && fabs(eta[ij]) < etaCut) {
+	  isGoodEvt = 1;
+	  if(pt[ij] > ptLead) {
+	    leadJet = ij;
+	    ptLead = pt[ij];
+	  }
+	  if(debug) cout << "LEAD JET: "<<  pt[ij] << " *******************************" << endl;
+
 	}
-	if(pt[ij] > ptCut && fabs(eta[ij]) < etaCut){
+	if(pt[ij] > ptCut && fabs(eta[ij]) < etaCut) { 
 	  pt_v.push_back(pt[ij]);	
 	  eta_v.push_back(eta[ij]);	
 	  phi_v.push_back(phi[ij]);
@@ -302,10 +257,10 @@ void jetPull_Pythia(Int_t startfile = 0,
       int NJets_Sel = pt_v.size();
 
       if(NJets_Sel < 2) {
-	if (debug) cout<<"This event had only 1 Jet"<<endl;
+      	if (debug) cout<<"This event had only 1 Jet"<<endl;
 	continue;	
       }
-      if(!isGoodEvt){continue;}
+      if(!isGoodEvt){  continue;  }
 
       if(debug) cout<< " \n ******* New Event ******** " << endl;
       if(debug) cout<< " ******* " << nentry << " ******** " << endl;
@@ -313,76 +268,101 @@ void jetPull_Pythia(Int_t startfile = 0,
       //reset maximum values
       eventCount++;
       evt++;
-      thrust_max = 0;
+      //EVENT LOOP  ====================================================
+      //runs through all of the jets, and calculates pull angle with just the leading jet
+      //for(Long64_t njet = 0; njet < NJets_Sel; ++njet){	
 
-      vector <double> px;
-      vector <double> py;
-      vector <double> pz;
+      //we will only need jet pull for the lead jet
+      //if(njet != leadJet) {
+      //  continue;
+      //}
       
-      for(Long64_t naxis = 0; naxis < NJets_Sel; ++naxis){
-	float axis_jet_pt = pt_v[naxis];
-	float axis_jet_eta = eta_v[naxis];
-	float axis_jet_phi = phi_v[naxis];
-      	px.push_back((double)axis_jet_pt * TMath::Cos(axis_jet_phi));
-	py.push_back((double)axis_jet_pt * TMath::Sin(axis_jet_phi));
-	pz.push_back((double)axis_jet_pt * TMath::SinH(axis_jet_eta));
+      r[leadJet].Set(0.0,0.0);
+      jetPull.Set(0.0,0.0);
+      if(debug) cout << "NEW JET: "<<  ngen << " *******************************" << endl;
+      
+      //PARTICLE LOOP STARTS HERE ===================================
+      //calculate jet pull for the individual jet
+      for(Long64_t npart = 0; npart < ngen; ++npart){
+	
+	//these files seem to have a lot of gen particles with pt = 0
+	//if ( genpt[npart] == 0) {
+	//  continue; 
+	//}
+
+	//if( genpt[npart] > pt_v[leadJet] ) {
+	  //  continue; 
+	  //	}
+	
+	//Delta R^2 cut
+	if((Double_t)(TMath::Power(TMath::Power(phi_v[leadJet] - genphi[npart],2) + TMath::Power(eta_v[leadJet] - geneta[npart],2),2)) > 0.3){
+	  continue;
+	}
+	
+	//calculate jet pull
+	TVector2 partAxis = TVector2(geneta[npart], genphi[npart]);
+	TVector2 jetAxis = TVector2(eta_v[leadJet], phi_v[leadJet]);
+	TVector2 newJetPull; 
+	r[leadJet].Set((partAxis.X() - jetAxis.X()),(partAxis.Y() - jetAxis.Y()));
+	//cout << r[leadJet].X() << " , " << r[leadJet].Y() << endl; 
+	
+	if(debug) cout << "NEW PART: Lead Jet pT " << pt_v[leadJet] << " Particle pT " << genpt[npart] << " ======> Lead Jet-->Subjet ri" <<  TMath::Sqrt(r[leadJet].X()*r[leadJet].X() + r[leadJet].Y()*r[leadJet].Y()) << endl;
+	
+	jetPullMag = TMath::Sqrt(r[leadJet].X()*r[leadJet].X() + r[leadJet].Y()*r[leadJet].Y()) * genpt[npart]/pt_v[leadJet];
+	if(debug) cout << "JET PULL MAG: " << jetPullMag << endl;
+	newJetPull.Set(jetPullMag*r[leadJet].X(), jetPullMag*r[leadJet].Y());
+	jetPull.Set(jetPull.X() + newJetPull.X(), jetPull.Y() + newJetPull.Y());
+ 	if(debug) cout << "JET PULL MAG w/ NEW PARTICLE: " << jetPullMag << endl;
+	
+	h_yParticle->Fill(geneta[npart], pThat_weight);
+	h_etaParticle->Fill(geneta[npart], pThat_weight);
+	
+      }//PARTICLE LOOP ENDS HERE ===========
+      
+      jetPullMag = TMath::Sqrt(jetPull.X()*jetPull.X() + jetPull.Y()*jetPull.Y());
+      
+      //only plot the leading jets
+      if(jetPullMag != 0){
+	h_jetPull->Fill(jetPullMag, pThat_weight);
+	h_jetPull_2d->Fill(jetPull.X(), jetPull.Y());
+	h_jetPull_3d->Fill(jetPull.X(), jetPull.Y(), pt_v[leadJet]);
+	h_leadingJetpT->Fill(pt_v[leadJet], pThat_weight);
+	h_leadingJetphi->Fill(phi_v[leadJet], pThat_weight);
       }
 
-      //EVENT LOOP  ====================================================
-      for(Long64_t njet = 0; njet < NJets_Sel; ++njet){	
+      //}//JET PULL LOOP ENDS HERE ===========================
+      
+      if(debug) cout << "Number of Jets Selected: " << NJets_Sel << endl;
+      
+      //calculate the pull angle of each subjet with the leading jet
+      //PULL ANGLE LOOP ====================================
+      for(Long64_t npull = 0; npull < NJets_Sel; ++npull){
 	
-	r.Set(0.0,0.0);
-	jetPull.Set(0.0,0.0);
-	if(debug) cout << "NEW JET: "<<  ngen << " *******************************" << endl;
+	if(debug) cout << "lead jet : " << leadJet << " Current Jet " << npull <<" NSelected : " << NJets_Sel << " Jet Pull " << jetPullMag << endl;
 
-	//PARTICLE LOOP STARTS HERE ===================================
-	for(Long64_t npart = 0; npart < ngen; ++npart){
-
-	  //Delta R^2 cut
-	  /*
-	  if((Double_t)(TMath::Power(genphi[npart],2) + TMath::Power(geneta[npart],2)) < 0.2){
-	    continue;
-	  }
-	  */
-	  if(debug) cout << "NEW PART: " << npart << " ================" << endl;
-	  if(debug) cout << "y: " << geny[npart] << endl;
-	  if(debug) cout << "azimuthal: " << genphi[npart] << endl;
-	  
-	  TVector2 partAxis = TVector2(geny[npart], genphi[npart]);
-	  TVector2 jetAxis = TVector2(y[njet], phi[njet]);
-	  TVector2 newJetPull; 
-	  r.Set((partAxis.X() - jetAxis.X()),(partAxis.Y() - jetAxis.Y()));
-	  if(debug) r.Print();
-	  if(debug) cout << genpt[ngen] << endl;
-
-	  jetPullMag = (r.X()*r.X() + r.Y()*r.Y())* genpt[ngen]/pt[njet];
-	  newJetPull = jetPullMag*r;
-	  jetPull.Set(jetPull.X() + newJetPull.X(), jetPull.Y() + newJetPull.Y());
-	  if(debug) cout << "JET PULL MAG: " << jetPullMag << endl;
-
-	  h_yParticle->Fill(geny[npart], pThat_weight);
-	  h_etaParticle->Fill(geneta[npart], pThat_weight);
-	  
-	}//PARTICLE LOOP ENDS HERE ===========
-
-	if(jetPullMag != 0){
-	  h_jetPull->Fill(jetPullMag, pThat_weight);
-	  h_jetPull_2d->Fill(jetPull.X(), jetPull.Y());
-	  Double_t weightNum = pt[njet]*pThat_weight;
-	  h_jetPull_3d->Fill(jetPull.X(), jetPull.Y(), pt[njet]);
+	//prevents double counting of lead jet
+	if(npull == leadJet) {
+	  continue;
 	}
-      }//end jet loop =======================
+	//prevents us from using jets will jet pull of zero --> why do we get these anyway?
+	if(jetPullMag == 0) {
+	  continue;
+	}
+	
+	// the vector defining the difference between the lead jet and this particular jet
+	TVector2 rpull; 
+	rpull.Set(eta_v[leadJet]-eta_v[npull], phi_v[leadJet]-phi_v[npull]);
+	
+	pullAngle = TMath::ACos((jetPull.X()*rpull.X() + jetPull.Y()*rpull.Y())/(TMath::Sqrt(jetPull.X()*jetPull.X() + jetPull.Y()*jetPull.Y()) * TMath::Sqrt(rpull.X()*rpull.X() + rpull.Y()*rpull.Y())));	
+	if(debug) cout << "pt_leadJet " << pt_v[leadJet] << " pt_mainJet " << pt_v[npull] << " Pull Angle: " << pullAngle << "\n arccos inside " << (jetPull.X()*rpull.X() + jetPull.Y()*rpull.Y()) << " / " << (TMath::Sqrt(jetPull.X()*jetPull.X() + jetPull.Y()*jetPull.Y()) * TMath::Sqrt(rpull.X()*rpull.X() + rpull.Y()*rpull.Y())) <<  endl;
+	
+	//fill pull angle and eta difference (for checking)
+
+	h_pullAngle->Fill(pullAngle, pThat_weight);
+	h_leadingJeteta->Fill(eta_v[leadJet] - eta_v[npull]);      
+      } // END OF PULL ANGLE LOOP ===============================
       
-      //fill all the maximum values before finishing
-      h_thrust->Fill(thrust_max);
-      h_min->Fill(thrust_min_max);
-      h_maj->Fill(thrust_maj_max);
-      
-      //h_jetPull->Fill(jetPull, pThat_weight);
-      h_thrust->Fill(thrust_max, pThat_weight);
-      h_min->Fill(thrust_min_max, pThat_weight);
-      h_maj->Fill(thrust_maj_max, pThat_weight);
-      
+      //fill values
       h_nref->Fill(nref);
       h_jetCount->Fill(NJets_Sel);
       h_weight->Fill(pThat,pThat_weight);
@@ -392,13 +372,7 @@ void jetPull_Pythia(Int_t startfile = 0,
       eta_v.clear();
       phi_v.clear();
       
-      px.clear();
-      py.clear();
-      pz.clear();
-      
-      //thrust_tree->Fill(); 
-      
-    }//end of event loop
+    }//end of event loop ==========================
     
     gROOT->GetListOfFiles()->Remove(file);
     gROOT->GetListOfFiles()->Remove(weight);
@@ -406,57 +380,33 @@ void jetPull_Pythia(Int_t startfile = 0,
     cout << "Events Selected: " << eventCount << endl;
     cout << "File Finished" << endl; 
     
-  }//end file loop
+  }//end file loop ================================
   
-  //SCALE HISTOGRAMS +++++++++++++++++++++++++++++++++++
-  //h_jetPull->Scale(1./h_jetPull->Integral());
-  h_thrust->Scale(1./h_thrust->Integral());
-  h_maj->Scale(1./h_maj->Integral());
-  h_min->Scale(1./h_min->Integral());
-  
-  //CREATE PLOTS VS. dN/dT +++++++++++++++++++++++
+  //SCALE HISTOGRAMS +++++++++++++++++++++++++++++++++++  
+  //CREATE PLOTS VS. dN/dT +++++++++++++++++++++++ (NOT CURRENTLY USED)
   TH1F * h_jetPullScaled = DivideByBinWidth(h_jetPull, "jetPull_scaled");
-  TH1F * h_T = DivideByBinWidth(h_thrust, "thrust_scaled");
-  TH1F * h_Tmaj = DivideByBinWidth(h_maj, "thrust_maj_scaled");
-  TH1F * h_Tmin = DivideByBinWidth(h_min, "thrust_min_scaled");
-  
+
   save_File->cd();
-  /* 
-  h_jetPullScaled->Print("base");
-  h_jetPull->Print("base"); 
-  h_T->Print("base");
-  h_Tmaj->Print("base");
-  h_Tmin->Print("base");
-  h_pT->Print("base");
-  h_pTcut->Print("base");
-  h_nref->Print("base");
-  h_jetCount->Print("base");
-  h_eta->Print("base");
-  h_phi->Print("base");
-  h_weight->Print("base");
-  h_yParticle->Print("base");
-  h_etaParticle->Print("base");
-  */
+  
   h_jetPullScaled->Write();
   h_jetPull->Write();
-  h_T->Write();
-  h_Tmaj->Write();
-  h_Tmin->Write();
-  h_thrust->Write();
-  h_maj->Write();
-  h_min->Write();
-  h_pT->Write();
+  h_leadingJetpT->Write();
   h_pTcut->Write();
   h_nref->Write();
   h_jetCount->Write();
-  h_eta->Write();
-  h_phi->Write();
+  h_leadingJeteta->Write();
+  h_leadingJetphi->Write();
   h_weight->Write();
   h_unweight->Write();
   h_yParticle->Write();
   h_etaParticle->Write();
   h_jetPull_2d->Write();
   h_jetPull_3d->Write();
+  h_pullAngle->Write();
+  h_30->Write();
+  h_60->Write();
+  h_90->Write();
+  h_120->Write();
   
   save_File->Write();
   save_File->Close();
@@ -468,4 +418,6 @@ void jetPull_Pythia(Int_t startfile = 0,
   cout<<"CPU time (min)  = "<<(Float_t)timer.CpuTime()/60<<endl;
   cout<<"Real time (min) = "<<(Float_t)timer.RealTime()/60<<endl;
     
-}//end of plot thrust
+}//end of plot jetPull
+
+ 
